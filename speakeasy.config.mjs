92603a4cdd351654d5f3a9880402389e674c74d3
nebrius <bryan.hughes@speakeasy.com>
2025-06-09 13:38:50 -0700

    Created config file

---
+++ speakeasy.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  spec: "./openapi.yaml",
+  output: {
+    pageOutDir: "./docs/api",
+    componentOutDir: "./src/components/speakeasy",
+    framework: "docusaurus",
+  },
+  display: {
+    showSchemasInNav: false,
+    showTypeSignatures: true,
+  },
+};
