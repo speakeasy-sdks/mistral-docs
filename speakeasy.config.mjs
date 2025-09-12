43fd48144b442cd781ebc5eeb9eaf0a31ec61710
nebrius <bryan.hughes@speakeasy.com>
2025-09-12 15:02:28 -0700

    Brought in latest changes

--- speakeasy.config.mjs
+++ speakeasy.config.mjs
@@ -2,15 +2,21 @@ export default {
   spec: "./openapi_with_overlays.yaml",
   output: {
     pageOutDir: "./docs/api",
-    componentOutDir: "./src/components/speakeasy",
     framework: "docusaurus",
   },
   display: {
     visibleResponses: "success",
-    expandTopLevelPropertiesOnPageLoad: true,
   },
-  tryItNow: {
-    npmPackageName: "@mistralai/mistralai",
-    sdkClassName: "Mistral",
-  },
+  tryItNow: [
+    {
+      language: "typescript",
+      sdkClassName: "Mistral",
+      packageName: "@mistralai/mistralai",
+    },
+    {
+      language: "python",
+      sdkClassName: "Mistral",
+      packageName: "mistralai",
+    },
+  ],
 };
